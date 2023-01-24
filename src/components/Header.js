6ee1a86daefda651d40e6896997ca2d628201431
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-01-24 16:08:40 +0100

    Sections for Tesla

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model Y</a>
+
+        <a href="#">Solar Roof</a>
+
+        <a href="#">Solar Panels</a>
+      </Menu>
+
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <CustomMenu>
+          <a href="#">Menu</a>
+        </CustomMenu>
+      </RightMenu>
+    </Container>
   );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 400;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+const RightMenu = styled.div`
+  display: flex;
+  a {
+    font-weight: 400;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+
+const CustomMenu = styled.div``;
